3596324fc3adfe55a6d3ec992512308dce12aa4e
AtanuDas2005 <iamatanudas2005@gmail.com>
2026-03-30 11:12:09 +0530

    Done update

--- train_model.py
+++ train_model.py
@@ -3,10 +3,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import pickle
 
-# Load dataset
 data = pd.read_csv("crop_dataset_smartcrop.csv")
 
-# Convert text values to numbers
 soil_map = {
 "loamy":0,
 "sandy":1,
@@ -23,21 +21,16 @@ season_map = {
 data["soil_type"] = data["soil_type"].map(soil_map)
 data["season"] = data["season"].map(season_map)
 
-# Features
 X = data[["soil_type","temperature","rainfall","season"]]
 
-# Label
 y = data["label"]
 
-# Split data
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
 
-# Train KNN model
 model = KNeighborsClassifier(n_neighbors=5)
 
 model.fit(X_train,y_train)
 
-# Save model
-pickle.dump(model, open("crop_model.pkl","wb"))
+pickle.dump(model, open("crop_model.pkl","wb")) #saves the model to a file named "crop_model.pkl"
 
 print("Model trained successfully")
